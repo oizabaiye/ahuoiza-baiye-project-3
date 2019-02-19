bc55108248747dcf4eca9b8f013291eda2d19910
Ahuoiza <oizabaiye@gmail.com>
2019-02-18 22:06:30 -0500

    finished quiz. media queries in progress

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
-//init - is most helpful with big applications and if you're doing namespacing
-
 $(document).ready(function(){
 
 	$('form').on('submit', function (event) {
 		event.preventDefault();
-		let optimistAnswer = $('input[name="optimist"]:checked').val();
-		let outdoorsyAnswer = $('input[name="outdoorsy"]:checked').val();
-		let dramaticAnswer = $('input[name="dramatic"]:checked').val();
+		const optimistAnswer = $('input[name="optimist"]:checked').val();
+		const outdoorsyAnswer = $('input[name="outdoorsy"]:checked').val();
+		const dramaticAnswer = $('input[name="dramatic"]:checked').val();
 
 		if ((optimistAnswer === 'true') && (outdoorsyAnswer === 'true') && (dramaticAnswer === 'true')){
 			$(".double-rainbow").removeClass("hidden").addClass("show");
@@ -25,14 +22,9 @@ $(document).ready(function(){
 			$(".leave-britney").removeClass("hidden").addClass("show");
 		} else if ((optimistAnswer === 'false') && (outdoorsyAnswer === 'false') && (dramaticAnswer === 'false')){
 			$(".snow-fail").removeClass("hidden").addClass("show");
-		} 
+		}
+
+		$('#submit-btn').hide();
+
 	});
 });
-
-/* 
-normal state - each div is on display: none. 
-there is a class called .show{display: block}
-
-
-
-*/
